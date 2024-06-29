f5c91f40ad85fd205e93bc16d809c061b112a198
suryansh318 <suryanshtejas@gmail.com>
2024-06-29 13:18:55 +0530

    added hyperlink for home

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand navbar-light fixed-top">
         <div class="container">
-            <a href="#" class="navbar-brand">Home</a>
+            <a href="/" class="navbar-brand">Home</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
