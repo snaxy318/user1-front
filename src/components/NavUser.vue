44c55b1e828fb8160f3c057e019bbeb8e40db202
suryansh318 <suryanshtejas@gmail.com>
2024-06-29 16:52:24 +0530

    add hyperlinks to navUser

--- src/components/NavUser.vue
+++ src/components/NavUser.vue
@@ -1,14 +1,14 @@
 <template>
     <nav class="navbar navbar-expand navbar-light fixed-top">
         <div class="container">
-            <a href="#" class="navbar-brand">User Details</a>
+            <a href="/user-details" class="navbar-brand">User Details</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Task Details</a>
+                        <a href="/task" class="nav-link">Task Details</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Create Task</a>
+                        <a href="/create-task" class="nav-link">Create Task</a>
                     </li>
                 </ul>
             </div>
