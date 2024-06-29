2c098b3d3a21692412a6d5bb4e0d49f8e48b05e5
suryansh318 <suryanshtejas@gmail.com>
2024-06-29 13:15:55 +0530

    added hyperlinks to navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,10 +5,10 @@
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Log in</a>
+                        <a href="./login" class="nav-link">Log in</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Sign Up</a>
+                        <a href="./signup" class="nav-link">Sign Up</a>
                     </li>
                 </ul>
             </div>
