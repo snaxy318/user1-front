b5f36c6e1b3cbb888d1776c00c21aaf15dbd9b79
suryansh318 <suryanshtejas@gmail.com>
2024-06-29 11:38:34 +0530

    added Nav.vue

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar navbar-expand navbar-light fixed-top">
+        <div class="container">
+            <a href="#" class="navbar-brand">Home</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Log in</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Sign UP</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Nav'
+    }
+</script>
